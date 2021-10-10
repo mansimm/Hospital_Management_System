5a7692927fd764195783bccaf2b3261a56661ff3
mansimm <mansimisal88@gmail.com>
2021-10-10 20:19:17 +0530

    Added Rest Api for hospital staff entity

--- hms/src/main/java/com/project/hms/HospitalManagementSystemApplication.java
+++ hms/src/main/java/com/project/hms/HospitalManagementSystemApplication.java
@@ -6,13 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HospitalManagementSystemApplication {
 
-	//main class
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
 	}
-	public void fun()
-	{
-		
-	}
-
+	
 }
---
+++ hms/src/main/java/com/project/hms/api/HospitalStaffApi.java
@@ -0,0 +1,32 @@
+package com.project.hms.api;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.hms.exception.HospitalStaffServiceException;
+import com.project.hms.model.HospitalStaffDto;
+import com.project.hms.service.HospitalStaffService;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value="HospitalStaff")
+public class HospitalStaffApi {
+
+	@Autowired
+	HospitalStaffService hospitalStaffService;
+	
+	@PostMapping(value="add/{hospitalStaffDto}")
+	public ResponseEntity<String> addHopitalStaff(@Valid @RequestBody HospitalStaffDto hospitalStaffDto) throws HospitalStaffServiceException
+	{
+		String s = hospitalStaffService.addHopitalStaff(hospitalStaffDto);
+		return new ResponseEntity<String>(s,HttpStatus.OK);
+	}
+}
