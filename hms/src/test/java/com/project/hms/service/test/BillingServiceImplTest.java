90be9819b130fd97fceee59f06804e6827a7f0d4
mansimm <mansimisal88@gmail.com>
2021-11-03 13:16:48 +0530

    Unit testing done for calculateTotalBill method

--- hms/src/test/java/com/project/hms/service/test/BillingServiceImplTest.java
+++ hms/src/test/java/com/project/hms/service/test/BillingServiceImplTest.java
@@ -16,8 +16,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.project.hms.entity.Address;
 import com.project.hms.entity.Diagnosis;
 import com.project.hms.entity.DiagnosticsConducted;
+import com.project.hms.entity.Medicine;
+import com.project.hms.entity.MedicineIssued;
 import com.project.hms.entity.Patient;
 import com.project.hms.exception.BillingServiceException;
+import com.project.hms.model.BillDto;
 import com.project.hms.model.Status;
 import com.project.hms.model.TypeOfBed;
 import com.project.hms.repository.DiagnosisRepo;
@@ -48,6 +51,9 @@ public class BillingServiceImplTest {
 	Address address;
 	Diagnosis diagnosis;
 	DiagnosticsConducted diagnosticsConducted;
+	MedicineIssued medicineIssued;
+	Medicine medicine; 
+	
 	public void init()
 	{
 		address = new Address(111,"xyz","abc");
@@ -58,8 +64,12 @@ public class BillingServiceImplTest {
 		diagnosis = new Diagnosis("Hemogram","Hemogram comprises of CBC and ESR",150.0);
 		diagnosis.setTestId(1);
 		diagnosticsConducted = new DiagnosticsConducted(1,  1,"Positive", "Need iron rich diet");
-		diagnosticsConducted.setDiagnosticsConductedId(1);		
+		diagnosticsConducted.setDiagnosticsConductedId(1);	
+		
+		medicine = new Medicine("Crosine",20,10.0);
+		medicine.setMedicineId(1);
 
+		medicineIssued = new MedicineIssued(1, 1, 2);
 	}
 	@Test
 	public void noOfDaysInHospitalOneDayTest() throws BillingServiceException
@@ -145,5 +155,33 @@ public class BillingServiceImplTest {
 		Double bill = billingServiceImpl.calculateDiagnisisBill(1);
 		Assertions.assertEquals(0.0, bill);
 	}
-	
+	@Test
+	public void calculateMedicineBillTest1()
+	{
+		init();
+		Mockito.when(medicineIssuedRepo.findByPatientId(1)).thenReturn(List.of(medicineIssued));
+		Mockito.when(medicineRepo.findRateByMedicineId(1)).thenReturn(10.0);
+		Double bill = billingServiceImpl.calculateMedicineBill(1);
+		Assertions.assertEquals(20.0, bill);
+	}
+	@Test
+	public void calculateTotalBillExceptionTest()
+	{
+		init();
+		Mockito.when(patientRepo.findById(1)).thenReturn(Optional.ofNullable(null));
+		Exception e = Assertions.assertThrows(BillingServiceException.class, ()->billingServiceImpl.calculateTotalBill(1));
+		Assertions.assertEquals("Patient not found", e.getMessage());
+	}
+//	@Test
+//	public void calculateTotalBillSuccessTest() throws BillingServiceException
+//	{
+//		init();
+//		Mockito.when(patientRepo.findById(1)).thenReturn(Optional.of(patient));
+//		Mockito.when(billingServiceImpl.calculateMedicineBill(1)).thenReturn(100.0);
+//		Mockito.when(billingServiceImpl.calculateDiagnisisBill(1)).thenReturn(100.0);
+//		Mockito.when(billingServiceImpl.calculateRoomBill(1)).thenReturn(100.0);
+//		Mockito.when(billingServiceImpl.applyGST(100.0)).thenReturn(0.0);
+//		BillDto bill = billingServiceImpl.calculateTotalBill(1);
+//		Assertions.assertEquals(bill.getTotalBill(), 300.0);
+//	}
 }
