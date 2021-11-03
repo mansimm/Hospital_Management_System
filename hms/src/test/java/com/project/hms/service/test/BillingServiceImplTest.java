32f0852c615c9384e3db929850a2295f683e0c17
mansimm <mansimisal88@gmail.com>
2021-11-03 12:49:43 +0530

    Unit testing done calculateDiagnisisBill method

--- hms/src/test/java/com/project/hms/service/test/BillingServiceImplTest.java
+++ hms/src/test/java/com/project/hms/service/test/BillingServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.project.hms.service.test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.project.hms.entity.Address;
+import com.project.hms.entity.Diagnosis;
+import com.project.hms.entity.DiagnosticsConducted;
 import com.project.hms.entity.Patient;
 import com.project.hms.exception.BillingServiceException;
 import com.project.hms.model.Status;
@@ -42,6 +46,8 @@ public class BillingServiceImplTest {
 	
 	Patient patient;
 	Address address;
+	Diagnosis diagnosis;
+	DiagnosticsConducted diagnosticsConducted;
 	public void init()
 	{
 		address = new Address(111,"xyz","abc");
@@ -49,6 +55,11 @@ public class BillingServiceImplTest {
 		patient = new Patient(123, "Tony", 70, LocalDate.now(), TypeOfBed.GeneralWard,
 				address , "Japan", "Sakura", Status.Active);
 		patient.setPatientId(1);
+		diagnosis = new Diagnosis("Hemogram","Hemogram comprises of CBC and ESR",150.0);
+		diagnosis.setTestId(1);
+		diagnosticsConducted = new DiagnosticsConducted(1,  1,"Positive", "Need iron rich diet");
+		diagnosticsConducted.setDiagnosticsConductedId(1);		
+
 	}
 	@Test
 	public void noOfDaysInHospitalOneDayTest() throws BillingServiceException
@@ -82,6 +93,57 @@ public class BillingServiceImplTest {
 		Double gst = billingServiceImpl.applyGST(5500.0);
 		Double required = 5500.0*18/100.0;
 		Assertions.assertEquals(required, gst);
+	}
+	@Test
+	public void calculateRoomBillGeneralWard() throws BillingServiceException
+	{
+		init();
+		patient.setDateOfAdmission(LocalDate.now());
+		patient.setDateOfDischarge(LocalDate.now().plusDays(5));
+		Mockito.when(patientRepo.findById(1)).thenReturn(Optional.of(patient));
+		Double bill = billingServiceImpl.calculateRoomBill(1);//2000.0/day
+		Assertions.assertEquals(2000.0*5, bill);
+	}
+	@Test
+	public void calculateRoomBillSemiSharing() throws BillingServiceException
+	{
+		init();
+		patient.setDateOfAdmission(LocalDate.now());
+		patient.setDateOfDischarge(LocalDate.now().plusDays(5));
+		patient.setTypeOfBed(TypeOfBed.SemiSharing);
+		Mockito.when(patientRepo.findById(1)).thenReturn(Optional.of(patient));
+		Double bill = billingServiceImpl.calculateRoomBill(1);//4000.0/day
+		Assertions.assertEquals(4000.0*5, bill);
+	}
+	@Test
+	public void calculateRoomBillSingleRoom() throws BillingServiceException
+	{
+		init();
+		patient.setDateOfAdmission(LocalDate.now());
+		patient.setDateOfDischarge(LocalDate.now().plusDays(5));
+		patient.setTypeOfBed(TypeOfBed.SingleRoom);
+		Mockito.when(patientRepo.findById(1)).thenReturn(Optional.of(patient));
+		Double bill = billingServiceImpl.calculateRoomBill(1);//8000.0/day
+		Assertions.assertEquals(8000.0*5, bill);
+	}
+	
+	@Test
+	public void calculateDiagnisisBillTest1()
+	{
+		init();
+		Mockito.when(diagnosticsConductedRepo.findByPatientId(1)).thenReturn(List.of(diagnosticsConducted));
+		Mockito.when(diagnosisRepo.findTestChargesByTestId(1)).thenReturn(150.0);
+		Double bill = billingServiceImpl.calculateDiagnisisBill(1);
+		Assertions.assertEquals(150.0, bill);
+	}
+	@Test
+	public void calculateDiagnisisBillTest2()
+	{
+		init();
+		Mockito.when(diagnosticsConductedRepo.findByPatientId(1)).thenReturn(new ArrayList());
+		//Mockito.when(diagnosisRepo.findTestChargesByTestId(1)).thenReturn(150.0);
+		Double bill = billingServiceImpl.calculateDiagnisisBill(1);
+		Assertions.assertEquals(0.0, bill);
 	}
 	
 }
